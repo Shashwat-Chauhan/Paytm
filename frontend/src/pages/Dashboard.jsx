9652f8ddc4064334410c0ca05c2aaeae95bd4535
Shashwat-Chauhan <144317840+Shashwat-Chauhan@users.noreply.github.com>
2024-06-26 21:48:42 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import axios from "axios"
 function Dashboard() {
   const [balance, setBalance] = useState(0);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/v1/account/balance",{
+    axios.get("https://paytm-tau6.onrender.com/api/v1/account/balance",{
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token") 
     }
